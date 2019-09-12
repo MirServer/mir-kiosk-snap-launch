108ccae4a96360874fe6be2bbc42dc4955683ec9
Alan Griffiths <alan@octopull.co.uk>
2019-09-12 15:00:40 +0100

    Script the build

--- build-with-plugs.sh
+++ build-with-plugs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed --in-place s'/%SNAP%/$SNAPCRAFT_PROJECT_NAME/' $SNAPCRAFT_PART_BUILD/bin/setup.sh
-sed --in-place s'/%PLUGS%/$*/' $SNAPCRAFT_PART_BUILD/bin/setup.sh
+sed --in-place s'/%SNAP%/'$SNAPCRAFT_PROJECT_NAME'/' $SNAPCRAFT_PART_BUILD/bin/setup.sh
+sed --in-place s'/%PLUGS%/'$*'/' $SNAPCRAFT_PART_BUILD/bin/setup.sh
 snapcraftctl build
 rm $SNAPCRAFT_PART_INSTALL/$(basename $0)
