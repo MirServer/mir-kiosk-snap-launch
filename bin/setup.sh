fd586f24a0e1c0ba1e1335d92342a80b060b28af
Alan Griffiths <alan@octopull.co.uk>
2021-07-30 09:33:23 +0100

    Connect Wayland on core

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 set -e
 
-for PLUG in %PLUGS%; do sudo snap connect %SNAP%:${PLUG}; done
+snap_connect() {
+  available_providers="$(snap interface $2 | sed -e '1,/slots:/d')"
+  for PROVIDER in snapd ubuntu-frame mir-kiosk; do
+     if echo "$available_providers" | grep --quiet "\- ${PROVIDER}"; then
+       sudo snap connect "$1:$2" "${PROVIDER}:$2"
+       return 0
+     fi
+  done
+  echo "Warning: Failed to connect '$2'. Please connect manually, available providers are:\n$available_providers"
+}
+
+for PLUG in %PLUGS%; do snap_connect %SNAP% ${PLUG}; done
