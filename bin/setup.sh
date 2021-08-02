249e70fd0db1786a33591d3f8086553a46243682
Alan Griffiths <alan@octopull.co.uk>
2021-08-02 15:14:28 +0100

    Make setup.sh more likely to work (#12)
    * Reduce spam in logs and treat missing interfaces as warnings, not fatal
    * Rework setup logic to connect plugs when there are multipl providers, or when Wayland is provided by a snap

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
 set -e
 
-for PLUG in %PLUGS%; do sudo snap connect %SNAP%:${PLUG}; done
+snap_connect_harder() {
+  # Note the available slot providers
+  available_providers="$(snap interface "$1" | sed -e '1,/slots:/d')"
+
+  # For wayland try some well known providers
+  if [ "wayland" = "$1" ]; then
+    for PROVIDER in ubuntu-frame mir-kiosk; do
+       if echo "$available_providers" | grep --quiet "\- ${PROVIDER}"; then
+         sudo snap connect "%SNAP%:$1" "${PROVIDER}:$1"
+         return 0
+       fi
+    done
+  fi
+
+  echo "Warning: Failed to connect '$1'. Please connect manually, available providers are:\n$available_providers"
+}
+
+for PLUG in %PLUGS%; do
+  sudo snap connect "%SNAP%:${PLUG}" 2> /dev/null || snap_connect_harder ${PLUG}
+done
