867ed3a2f0925fa81119d68b737d926b3ef6a0d7
Alan Griffiths <alan@octopull.co.uk>
2021-08-16 14:00:42 +0100

    Do more than one replacement per like

--- build-with-plugs.sh
+++ build-with-plugs.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
 
-sed --in-place s'/%SNAP%/'$SNAPCRAFT_PROJECT_NAME'/' $SNAPCRAFT_PART_BUILD/bin/setup.sh
-sed --in-place s'/%PLUGS%/'"$*"'/' $SNAPCRAFT_PART_BUILD/bin/setup.sh
-sed --in-place s'/%SNAP%/'$SNAPCRAFT_PROJECT_NAME'/' $SNAPCRAFT_PART_BUILD/bin/wayland-launch
-sed --in-place s'/%PLUGS%/'"$*"'/' $SNAPCRAFT_PART_BUILD/bin/wayland-launch
+sed --in-place s'/%SNAP%/'$SNAPCRAFT_PROJECT_NAME'/g' $SNAPCRAFT_PART_BUILD/bin/setup.sh
+sed --in-place s'/%PLUGS%/'"$*"'/g' $SNAPCRAFT_PART_BUILD/bin/setup.sh
+sed --in-place s'/%SNAP%/'$SNAPCRAFT_PROJECT_NAME'/g' $SNAPCRAFT_PART_BUILD/bin/wayland-launch
+sed --in-place s'/%PLUGS%/'"$*"'/g' $SNAPCRAFT_PART_BUILD/bin/wayland-launch
 
 snapcraftctl build
 rm $SNAPCRAFT_PART_INSTALL/$(basename $0)
